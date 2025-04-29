b8196bed7b4a74094fc1c9be6e3db58ba4197667
Ivan-gif-boop <Ivan.wachira@strathmore.edu>
2025-04-29 09:47:55 +0300

    Revert "All the challenges"
    This reverts commit b4b094e997df03fb5927c80b72b70f325988949c.

--- Greeter.java
+++
@@ -1,8 +0,0 @@
-public class Greeter {
-    void Greet(){
-        System.out.println("Hi there!");
-    }
-    public static void main(String[] args) {
-        new Greeter().Greet();
-    }
-}
--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # OOP
 All my class codes in Java
-WACHIRA IVAN MUREITHI- 176830
